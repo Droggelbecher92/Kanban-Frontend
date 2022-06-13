33d853f33ba9b8ab69b91ea029148782fd9cfb4d
Droggelbecher92 <thomaskittlaus@gmail.com>
2022-06-13 10:29:32 +0200

    basic template

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,49 @@ import React from 'react';
 import './App.css';
 
 export default function App() {
-  return (
-    <div>
-      Hallo
-    </div>
-  );
+    return (
+        <div>
+            <div>
+                <h1>Kanban-Dude</h1>
+            </div>
+            <div>
+                <input type="text"/>
+                <input type="text"/>
+                <button>ok</button>
+            </div>
+            <div>
+                <div>
+                    Status1
+                    <div>
+                        <p>Task</p>
+                        <p>Beschreibung</p>
+                        <button>ok</button>
+                        <button>ok</button>
+                        <button>ok</button>
+                    </div>
+                </div>
+                <div>
+                    Status2
+                    <div>
+                        <p>Task</p>
+                        <p>Beschreibung</p>
+                        <button>ok</button>
+                        <button>ok</button>
+                        <button>ok</button>
+                    </div>
+                </div>
+                <div>
+                    Status3
+                    <div>
+                        <p>Task</p>
+                        <p>Beschreibung</p>
+                        <button>ok</button>
+                        <button>ok</button>
+                        <button>ok</button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
 }
